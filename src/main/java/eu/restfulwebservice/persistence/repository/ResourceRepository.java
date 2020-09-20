7ea6d7253dd20677a59e37eb50d243667230eb94
Marin Kakarov <60268660+MrinAK@users.noreply.github.com>
2020-09-20 18:19:45 +0300

    some test

--- src/main/java/eu/restfulwebservice/persistence/repository/ResourceRepository.java
+++ src/main/java/eu/restfulwebservice/persistence/repository/ResourceRepository.java
@@ -1,9 +1,9 @@
-package eu.restfulwebservice.persistence.repository;
-
-import eu.restfulwebservice.persistence.entity.Resource;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResourceRepository extends JpaRepository<Resource, Long> {
-}
+//package eu.restfulwebservice.persistence.repository;
+//
+//import eu.restfulwebservice.persistence.entity.Resource;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface ResourceRepository extends JpaRepository<Resource, Long> {
+//}
