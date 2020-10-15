b58459cf25070953b329a5d2dbca7849d37c4ca8
Marin Kakarov <60268660+MrinAK@users.noreply.github.com>
2020-10-15 16:26:38 +0300

    save

--- src/main/java/eu/restfulwebservice/controller/ResourceController.java
+++ src/main/java/eu/restfulwebservice/controller/ResourceController.java
@@ -2,14 +2,14 @@ package eu.restfulwebservice.controller;
 
 import eu.restfulwebservice.service.ResourceService;
 import eu.restfulwebservice.service.dto.ResourceDTO;
+import eu.restfulwebservice.service.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/v1/resources")
 @RestController
@@ -26,6 +26,11 @@ public class ResourceController {
     public ResponseEntity<Void> create(@RequestBody @Validated ResourceDTO resourceDTO){
         resourceService.create(resourceDTO);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public List<ResourceDTO> getAll() {
+        return resourceService.findAll();
     }
 
 }
--- src/main/java/eu/restfulwebservice/service/ResourceService.java
+++ src/main/java/eu/restfulwebservice/service/ResourceService.java
@@ -3,8 +3,12 @@ package eu.restfulwebservice.service;
 import eu.restfulwebservice.persistence.entity.Resource;
 import eu.restfulwebservice.persistence.repository.ResourceRepository;
 import eu.restfulwebservice.service.dto.ResourceDTO;
+import eu.restfulwebservice.service.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ResourceService {
@@ -23,5 +27,14 @@ public class ResourceService {
                 resourceDTO.getRoleId(),
                 resourceDTO.getResourceName());
         resourceRepository.save(resource);
+    }
+
+    public List<ResourceDTO> findAll() {
+        return resourceRepository.findAll()
+                .stream()
+                .map(resource -> new ResourceDTO(resource.getResourceId(),
+                        resource.getRoleId(),
+                        resource.getResourceName()))
+                .collect(Collectors.toList());
     }
 }
