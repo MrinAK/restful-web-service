7d5bbe8a35646aafc69eac39ae927b920ca6f67f
Marin Kakarov <marinkakarov@yahoo.com>
2020-10-25 15:40:44 +0200

    save

--- src/main/java/eu/restfulwebservice/controller/UserController.java
+++ src/main/java/eu/restfulwebservice/controller/UserController.java
@@ -5,7 +5,6 @@ import eu.restfulwebservice.service.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
