4b3109feb0f1205aece03bac1d765d6f5d21ebd2
Marin Kakarov <60268660+MrinAK@users.noreply.github.com>
2020-11-22 00:37:24 +0200

    Delete RoleRepository.java

--- Restful-Web-Service/src/main/java/eu/restfulwebservice/persistence/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package eu.restfulwebservice.persistence.repository;
-
-import eu.restfulwebservice.persistence.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
