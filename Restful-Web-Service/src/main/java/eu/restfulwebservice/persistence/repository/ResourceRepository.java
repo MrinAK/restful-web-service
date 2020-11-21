7d9988d3b5880834317805952a4dbda4fa8965bc
Marin Kakarov <60268660+MrinAK@users.noreply.github.com>
2020-11-22 00:37:34 +0200

    Delete ResourceRepository.java

--- Restful-Web-Service/src/main/java/eu/restfulwebservice/persistence/repository/ResourceRepository.java
+++
@@ -1,9 +0,0 @@
-package eu.restfulwebservice.persistence.repository;
-
-import eu.restfulwebservice.persistence.entity.Resource;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResourceRepository extends JpaRepository<Resource, Long> {
-}
